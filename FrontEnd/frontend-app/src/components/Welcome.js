a818127b28e92339a87e561019f314b8c027ed62
Alan Lam <s3436174@student.rmit.edu.au>
2020-08-13 20:28:54 +1000

    Create components folder and create AGME title

---
+++ FrontEnd/frontend-app/src/components/Welcome.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+// function Welcome() {
+//     return <h1>AGME Booking Solutions</h1>
+// }
+
+const Welcome = () => <h1>AGME Booking Solutions</h1>
+
+export default Welcome
