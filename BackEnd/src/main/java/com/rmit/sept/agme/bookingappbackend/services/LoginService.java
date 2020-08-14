4e25fc44580dfc7ec5c7d30019214f32b01514a8
S3784450 <s3784450@student.rmit.edu.au>
2020-08-14 21:18:46 +1000

    Initial adding of LoginService service

---
+++ BackEnd/src/main/java/com/rmit/sept/agme/bookingappbackend/services/LoginService.java
@@ -0,0 +1,10 @@
+package com.rmit.sept.agme.bookingappbackend.services;
+
+import com.rmit.sept.agme.bookingappbackend.model.User;
+import com.rmit.sept.agme.bookingappbackend.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LoginService {
+}
