2a1bb9e1d9f3aefded062ea6f1411400455c095d
S3784450 <s3784450@student.rmit.edu.au>
2020-08-15 21:33:55 +1000

    Initial creation of LoginService and test. Dont know how to make tests run with repositories though, keep on getting errors.

--- BackEnd/src/main/java/com/rmit/sept/agme/bookingappbackend/repositories/UserRepository.java
+++ BackEnd/src/main/java/com/rmit/sept/agme/bookingappbackend/repositories/UserRepository.java
@@ -1,11 +1,15 @@
 package com.rmit.sept.agme.bookingappbackend.repositories;
 
 import com.rmit.sept.agme.bookingappbackend.model.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User, String> {
 
     @Override
     Iterable<User> findAllById(Iterable<String> iterable);
+
+    @Query("SELECT COUNT(u) FROM User u")
+    int countUsers();
 
 }
--- BackEnd/src/main/java/com/rmit/sept/agme/bookingappbackend/services/LoginService.java
+++ BackEnd/src/main/java/com/rmit/sept/agme/bookingappbackend/services/LoginService.java
@@ -5,6 +5,53 @@ import com.rmit.sept.agme.bookingappbackend.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
+//import java.util.Iterator;
+//import java.util.List;
+
 @Service
 public class LoginService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    /*
+        Incomplete; I'm not sure if a standalone method like this is needed, feel free
+        to delete if you have no use for it/no reason to warrant completion.
+        - Matt D
+     */
+//    public List<User> findAllByUsername(String username) {
+//        Iterator<User> allUsers = userRepository.findAll().iterator();
+//
+//        while (allUsers.hasNext()) {
+//            if(allUsers.next().getUsername().equals(username)) {
+//
+//            }
+//        }
+//    }
+
+    /**
+     * Validates inputted login credentials
+     * @param username Username entered in login form
+     * @param password Password entered in login form
+     * @return true if the entered login information has been successfully validated
+     */
+    public boolean validateLogin(String username, String password) {
+        boolean validated = false;
+
+        if (userRepository.countUsers() >= 5) {
+            Optional<User> userOptional = userRepository.findById(username);
+
+            if(userOptional.isPresent()) {
+                User foundUser = userOptional.get();
+
+                if (foundUser.getPassword().equals(password)) {
+                    validated = true;
+                }
+            }
+        }
+
+        return validated;
+    }
 }
