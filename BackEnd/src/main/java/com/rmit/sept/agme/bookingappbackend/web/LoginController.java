85dd1c692c60d44101f1bda47c4b65c1ffe6bb34
s3793336 <s3793336@student.rmit.edu.au>
2020-08-24 19:07:40 +1000

    Functional LoginController

--- BackEnd/src/main/java/com/rmit/sept/agme/bookingappbackend/web/LoginController.java
+++ BackEnd/src/main/java/com/rmit/sept/agme/bookingappbackend/web/LoginController.java
@@ -1,29 +1,34 @@
 package com.rmit.sept.agme.bookingappbackend.web;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.rmit.sept.agme.bookingappbackend.model.User;
 import com.rmit.sept.agme.bookingappbackend.services.LoginService;
 import com.rmit.sept.agme.bookingappbackend.services.UserService;
+import jdk.nashorn.internal.parser.JSONParser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/login")
-@CrossOrigin(origins = "http://localhost:3030")
+@CrossOrigin(origins = "http://localhost:3000")
 public class LoginController {
     @Autowired
     private LoginService loginService;
 
-    @PostMapping("")
-    public ResponseEntity<?> validate(@Valid @RequestBody String username,@Valid @RequestBody String password, BindingResult result) {
+    @PostMapping(value = "/login")
+    public ResponseEntity<?> validate(@RequestBody String loginDetails, BindingResult result) throws JsonProcessingException {
 
         if (result.hasErrors()) {
             Map<String, String> errorMap = new HashMap<>();
@@ -31,7 +36,13 @@ public class LoginController {
                 return new ResponseEntity<List<FieldError>>(result.getFieldErrors(), HttpStatus.BAD_REQUEST);
             }
         }
-        Boolean login = loginService.validateLogin(username, password, new User());
+        ObjectMapper m = new ObjectMapper();
+        JsonNode j = m.readTree(loginDetails);
+        String username = j.get("username").asText();
+        String password = j.get("password").asText();
+        Boolean login = loginService.validateLogin(username, password);
+
+
         return new ResponseEntity<Boolean>(login, HttpStatus.CREATED);
     }
 
