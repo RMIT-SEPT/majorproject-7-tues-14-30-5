933fc891c7961682c45d56194ca43715ffde4f76
S3784450 <s3784450@student.rmit.edu.au>
2020-09-11 17:26:22 +1000

    Added initial booking request for controller - Will work on it more after unit testing for other services has been completed

---
+++ BackEnd/src/main/java/com/rmit/sept/agme/bookingappbackend/requests/CreateBookingRequest.java
@@ -0,0 +1,11 @@
+package com.rmit.sept.agme.bookingappbackend.requests;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+/**
+ * Class for request '/api/booking/create'
+ */
+public class CreateBookingRequest {
+    
+}
