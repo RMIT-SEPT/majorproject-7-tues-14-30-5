289a6ca484ccbd00212fcc83e269c4178ee1e1a3
Alan Lam <s3436174@student.rmit.edu.au>
2020-09-12 18:00:41 +1000

    Update mapping and cross origin for UserController
    Changed the mapping and cross origin address to allow communication
    between front end and back end for a customer registration.

--- BackEnd/src/main/java/com/rmit/sept/agme/bookingappbackend/web/UserController.java
+++ BackEnd/src/main/java/com/rmit/sept/agme/bookingappbackend/web/UserController.java
@@ -2,18 +2,16 @@ package com.rmit.sept.agme.bookingappbackend.web;
 
 import com.rmit.sept.agme.bookingappbackend.exceptions.UserException;
 import com.rmit.sept.agme.bookingappbackend.model.User;
-import com.rmit.sept.agme.bookingappbackend.services.UserService;
 import com.rmit.sept.agme.bookingappbackend.requests.UpdateDetailsRequest;
-
-import org.aspectj.weaver.ResolvedPointcutDefinition;
+import com.rmit.sept.agme.bookingappbackend.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
@@ -22,7 +20,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/user")
+@CrossOrigin(origins = "http://localhost:3000")
 public class UserController {
 
     @Autowired
@@ -34,7 +32,7 @@ public class UserController {
      * @param result Validation results
      * @return ResponseEntity of added user if successful - Otherwise field errors or exception message
      */
-    @PostMapping("/registration")
+    @PostMapping(value = "/api/registration")
     public ResponseEntity<?> createNewUser(@Valid @RequestBody User user, BindingResult result) {
 
         if (result.hasErrors()) {
