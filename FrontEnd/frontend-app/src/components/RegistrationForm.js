574a05f5a9c624eba9ca8ff86894166a9a4d1ee2
Alan Lam <s3436174@student.rmit.edu.au>
2020-09-12 14:32:09 +1000

    Complete html registation form

--- FrontEnd/frontend-app/src/components/RegistrationForm.js
+++ FrontEnd/frontend-app/src/components/RegistrationForm.js
@@ -4,6 +4,11 @@ import axios from 'axios'
 export default class RegistrationForm extends Component {
 
     state = {
+        username:'',
+        password:'',
+        verifyPassword:'',
+        name:'',
+
     }
 
     render() {
@@ -13,7 +18,9 @@ export default class RegistrationForm extends Component {
                     <label>Username: <input type='text' name='username' onChange={this.handleChange} /></label><br />
                     <label>Password: <input type='text' name='password' onChange={this.handleChange} /></label><br />
                     <label>Re-enter password: <input type='text' name='verifyPassword' onChange={this.handleChange} /></label><br />
-                    <label>Name: <input type='text' name='username' onChange={this.handleChange} /></label><br />
+                    <label>Name: <input type='text' name='name' onChange={this.handleChange} /></label><br />
+                    <label>Address: <input type='text' name='address' onChange={this.handleChange} /></label><br />
+                    <label>Contact No: <input type='text' name='contactNo' onChange={this.handleChange} /></label><br />
                     <button type ='submit'>Register</button>
                 </form>
             </div>
