f6e03779b71c5b67e13893220f7e68aaab4f45aa
Alan Lam <s3436174@student.rmit.edu.au>
2020-09-12 17:19:43 +1000

    Implement functional registration page

--- FrontEnd/frontend-app/src/components/RegistrationForm.js
+++ FrontEnd/frontend-app/src/components/RegistrationForm.js
@@ -11,27 +11,20 @@ export default class RegistrationForm extends Component {
             verifyPassword:'',
             name:'',
             address:'',
-            contactNo:''
+            contactNo:'',
+            role:'customer'
         };
+
     }
 
-    // state = {
-    //     username:'',
-    //     password:'',
-    //     verifyPassword:'',
-    //     name:'',
-    //     address:'',
-    //     contactNo:''
-
-    // }
-
-    handleChange = event => {
+    handleChange = (event) => {
         this.setState({
             [event.target.name]: event.target.value
         });
     }
 
-    handleSubmit = event => {
+    handleSubmit = (event) => {
+        
         event.preventDefault();
 
         const user = {
@@ -39,40 +32,64 @@ export default class RegistrationForm extends Component {
             password: this.state.password,
             name: this.state.name,
             address: this.state.address,
-            contactNo: this.state.contactNo
+            contactNo: this.state.contactNo,
+            role: this.state.role
         };
-
-        const {password, verifyPassword} = this.state;
-
-        if (password !== verifyPassword) {
+        
+        const passwordCheck = {password: this.state.password, verifyPassword: this.state.verifyPassword};
+        
+        if (passwordCheck.password !== passwordCheck.verifyPassword) {
             alert("Passwords don't match!");
-
+            
         } else {
-            axios.post('http://localhost:8080/api/user/registration', {user})
-                .then(res => {
-                    console.log(res);
-                    console.log(res.data);
-                })
 
+            axios.post('http://localhost:8080/api/registration', user)
+            .then(res => {
+                console.log(res);
+                console.log(res.data);
+            })
+            
         }
 
     }
-
-
-
+        
     render() {
+
+        const {username, password, name, verifyPassword, address, contactNo} = this.state
+
+
         return (
             <div>
+                <h1>Register new customer</h1>
                 <form onSubmit={this.handleSubmit}>
-                    <label>Username: <input type='text' name='username' onChange={this.handleChange} /></label><br />
-                    <label>Password: <input type='password' name='password' onChange={this.handleChange} /></label><br />
-                    <label>Re-enter password: <input type='password' name='verifyPassword' onChange={this.handleChange} /></label><br />
-                    <label>Name: <input type='text' name='name' onChange={this.handleChange} /></label><br />
-                    <label>Address: <input type='text' name='address' onChange={this.handleChange} /></label><br />
-                    <label>Contact No: <input type='text' name='contactNo' onChange={this.handleChange} /></label><br />
-                    <button type ='submit'>Register</button>
+                    <div className='form-group'>
+                        <label>Username:</label>
+                        <input type='text' className='form-control' id='username"' placeholder='Enter username' name='username' value={username} onChange={this.handleChange} />
+                    </div>
+                    <div className='form-group'>
+                        <label>Password:</label>
+                        <input type='password' className='form-control' id='password"' placeholder='Enter password' name='password' value={password} onChange={this.handleChange} />
+                    </div>
+                    <div className='form-group'>
+                        <label>Re-enter Password:</label>
+                        <input type='password' className='form-control' id='verifyPassword"' placeholder='Verify password' name='verifyPassword' value={verifyPassword} onChange={this.handleChange} />
+                    </div>
+                    <div className='form-group'>
+                        <label>Name:</label>
+                        <input type='text' className='form-control' id='name"' placeholder='Enter name' name='name' value={name} onChange={this.handleChange} />
+                    </div>
+                    <div className='form-group'>
+                        <label>Address:</label>
+                        <input type='text' className='form-control' id='address"' placeholder='Enter address' name='address' value={address} onChange={this.handleChange} />
+                    </div>
+                    <div className='form-group'>
+                        <label>Contact Number:</label>
+                        <input type='text' className='form-control' id='contactNo"' placeholder='Enter contact' name='contactNo' value={contactNo} onChange={this.handleChange} />
+                    </div>
+                    <button type='submit' className='btn-default'>Submit</button>
                 </form>
             </div>
         )
     }
+
 }
