c5825145d3e82c3af043b3ff065497066cc32b66
Alan Lam <s3436174@student.rmit.edu.au>
2020-09-12 23:15:17 +1000

    Update User class to reflect suggestions

--- BackEnd/src/main/java/com/rmit/sept/agme/bookingappbackend/model/User.java
+++ BackEnd/src/main/java/com/rmit/sept/agme/bookingappbackend/model/User.java
@@ -12,14 +12,13 @@ import java.util.Date;
  public class User {
 
     @Id
-    @Size(min = 2, max = 20, message = "Name must be longer than 6 characters")
+    @Size(min = 6, max = 20, message = "Name must be longer than 6 characters")
     private String username;
     @Size(min = 6, max = 20, message = "Password must be longer than 6 characters")
     @NotBlank(message = "Password required")
     private String password;
     @NotBlank(message = "Name required")
     private String name;
-    @NotBlank(message = "Address required")
     private String address;
     @NotBlank(message = "Contact number required")
     private String contactNo;
@@ -31,6 +30,15 @@ import java.util.Date;
     private Date updated_At;
 
     public User() {}
+
+    public User(String username, String password, String name, String contactNo, String role) {
+        this.username = username;
+        this.password = password;
+        this.name = name;
+        this.contactNo = contactNo;
+        this.role = role;
+
+    }
 
     public String getUsername() {
         return username;
