892df06d9537b243d18a29363f3ad7d6681ad77d
Gaveen Amarapala <s3766914@student.rmit.edu.au>
2020-09-15 00:44:40 +1000

    Adding Unit Tests for User Service Testing - @GaveenA

--- BackEnd/src/test/java/com/rmit/sept/agme/bookingappbackend/services/UserServiceTests.java
+++ BackEnd/src/test/java/com/rmit/sept/agme/bookingappbackend/services/UserServiceTests.java
@@ -5,6 +5,7 @@ import com.rmit.sept.agme.bookingappbackend.model.User;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.TransactionSystemException;
 import org.testng.Assert;
 
 import static org.junit.Assert.*;
@@ -25,12 +26,23 @@ public class UserServiceTests {
     private User user1;
     private User user2;
     private User user3;
+    private User user4;
+    private User user5;
+    private User user6;
+    private User user7;
+    private User user8;
 
     @BeforeAll
     void setUp() {
         user1 = new User("Username","Password", "PersonName", "0404040404", "customer");
         user2 = new User("Username","Password", "PersonName", "Hello", "customer");
         user3 = new User("Username","Password", "PersonName", "0404040404", "customa");
+
+        user4 = new User("","Password", "PersonName", "0404040404", "customer");
+        user5 = new User("Username","", "PersonName", "0404040404", "customer");
+        user6 = new User("Username1234567890123","Password", "PersonName", "0404040404", "customer");
+        user7 = new User("Username","Password", "", "0404040404", "customer");
+        user8 = new User("Username","Password", "PersonName", "", "customer");
     }
 
     @AfterEach
@@ -95,7 +107,73 @@ public class UserServiceTests {
     }
 
     @Test
-    @DisplayName("updateUser: Updating user details")
+    @DisplayName("addUser: Adding New user with empty Username - Raises Nested Exception")
+    void addingUserWithEmptyUsername() {
+        Exception exception = assertThrows(TransactionSystemException.class, ()->userService.addUser(user4));
+
+        String expectedMessage = "Could not commit JPA transaction; nested exception is javax.persistence.RollbackException: Error while committing the transaction";
+        String actualMessage = exception.getMessage();
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    @DisplayName("addUser: Adding New user with empty Password - Raises Nested Exception")
+    void addingUserWithEmptyPassword() {
+        Exception exception = assertThrows(TransactionSystemException.class, ()->userService.addUser(user5));
+
+        String expectedMessage = "Could not commit JPA transaction; nested exception is javax.persistence.RollbackException: Error while committing the transaction";
+        String actualMessage = exception.getMessage();
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+
+    @Test
+    @DisplayName("addUser: Adding New user with Username more than 20 Characters - Raises Nested Exception")
+    void addingUser_withLongUsername_raisesException() {
+        Exception exception = assertThrows(TransactionSystemException.class, ()->userService.addUser(user6));
+
+        String expectedMessage = "Could not commit JPA transaction; nested exception is javax.persistence.RollbackException: Error while committing the transaction";
+        String actualMessage = exception.getMessage();
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+
+    @Test
+    @DisplayName("addUser: Adding New user with Empty Name - Raises Nested Exception")
+    void addingUser_withEmptyName_raisesNestedException() {
+        Exception exception = assertThrows(TransactionSystemException.class, ()->userService.addUser(user7));
+
+        String expectedMessage = "Could not commit JPA transaction; nested exception is javax.persistence.RollbackException: Error while committing the transaction";
+        String actualMessage = exception.getMessage();
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+
+    @Test
+    @DisplayName("addUser: Adding New user with Empty Contact Number - Raises Exception")
+    void addingUser_withEmptyContactNumber_raisesException() {
+        Exception exception = assertThrows(UserException.class, ()->userService.addUser(user8));
+
+        String expectedMessage = " is not a valid contact number.";
+        String actualMessage = exception.getMessage();
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    @Test
+    @DisplayName("updateUser: Updating user details - Checking that Updated user details different from Orignal")
     void _1_updateUser_false_user1() {
         userService.addUser(user1);
         User newDetailsUser = userService.updateUser("Username", "newPassword", "newName",
@@ -104,8 +182,18 @@ public class UserServiceTests {
     }
 
     @Test
+    @DisplayName("updateUser: Testing that User Details get updated correctly")
+    void _2_updateUser_true_updatedUser() {
+        userService.addUser(user1);
+        User newDetailsUser = userService.updateUser("Username", "newPassword", "newName",
+                "newLastName", "newAddress", "0505050505");
+        User updatedUser = userService.findUser(user1.getUsername());
+        Assert.assertTrue(sameUser(updatedUser, newDetailsUser));
+    }
+
+    @Test
     @DisplayName("updateUser: Password too short")
-    void _2_updateUser_passwordCheckLessThan6CharsThrowsException_thenSucceedsMessage() {
+    void _3_updateUser_passwordCheckLessThan6CharsThrowsException_thenSucceedsMessage() {
         userService.addUser(user1);
         Exception exception = assertThrows(UserException.class, ()->
                 userService.updateUser("Username", "pw", "newName",
@@ -118,8 +206,22 @@ public class UserServiceTests {
     }
 
     @Test
+    @DisplayName("updateUser: Password equals Max accepted length of 20 characters - boundary value test")
+    void updateUser_newPassword_20_Char_MaxValue_thenSuccessfully_Updated() {
+        userService.addUser(user1);
+
+        User newDetailsUser = userService.updateUser("Username", "passwordMaxAcceptVal", "newName",
+                        "newLastName", "newAddress", "0505050505");
+
+        User updatedUser = userService.findUser(user1.getUsername());
+        Assert.assertEquals(newDetailsUser.getPassword(),updatedUser.getPassword());
+
+    }
+
+
+    @Test
     @DisplayName("updateUser: Password too long")
-    void _3_updateUser_passwordCheckMoreThan20CharsThrowsException_thenSucceedsMessage() {
+    void _4_updateUser_passwordCheckMoreThan20CharsThrowsException_thenSucceedsMessage() {
         userService.addUser(user1);
         Exception exception = assertThrows(UserException.class, ()->
                 userService.updateUser("Username", "newpasswordishellalongandshouldbelong", "newName",
@@ -133,7 +235,7 @@ public class UserServiceTests {
 
     @Test
     @DisplayName("updateUser: First name is blank")
-    void _4_updateUser_firstNameIsEmptyThrowsException_thenSucceedsMessage() {
+    void _5_updateUser_firstNameIsEmptyThrowsException_thenSucceedsMessage() {
         userService.addUser(user1);
         Exception exception = assertThrows(UserException.class, ()->
                 userService.updateUser("Username", "newPassword", "",
@@ -146,8 +248,23 @@ public class UserServiceTests {
     }
 
     @Test
+    @DisplayName("updateUser: Updated Password is Blank")
+    void updateUser_Password_isEmpty_ThrowsException_thenSucceedsMessage() {
+        userService.addUser(user1);
+        Exception exception = assertThrows(UserException.class, ()->
+                userService.updateUser("Username", "", "Moraless",
+                        "newLastName", "newAddress", "0505050505"));
+
+        String expectedMessage = "Information is not valid";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+
+    }
+
+    @Test
     @DisplayName("updateUser: Last name is empty and OK")
-    void _5_updateUser_lastNameIsEmpty_validAndTrue() {
+    void _6_updateUser_lastNameIsEmpty_validAndTrue() {
         userService.addUser(user1);
         User newUser = userService.updateUser("Username", "newPassword", "newName",
                 "", "newAddress", "0505050505");
@@ -161,7 +278,7 @@ public class UserServiceTests {
 
     @Test
     @DisplayName("updateUser: Address is empty and OK")
-    void _6_updateUser_addressIsEmpty_validAndTrue() {
+    void _7_updateUser_addressIsEmpty_validAndTrue() {
         userService.addUser(user1);
         User newUser = userService.updateUser("Username", "newPassword", "newFirstName",
                 "newLastName", "", "0505050505");
@@ -175,7 +292,7 @@ public class UserServiceTests {
 
     @Test
     @DisplayName("updateUser: Contact Number is incorrect")
-    void _7_updateUser_contactNoIsIncorrectThrowsException_thenSucceedsMessage() {
+    void _8_updateUser_contactNoIsIncorrectThrowsException_thenSucceedsMessage() {
         userService.addUser(user1);
         Exception exception = assertThrows(UserException.class, ()->
                 userService.updateUser("Username", "newPassword", "newFirstName",
@@ -188,8 +305,23 @@ public class UserServiceTests {
     }
 
     @Test
+    @DisplayName("updateUser: Contact Number is Empty and raises exception")
+    void updateUser_contactNoIsEmptyThrowsException() {
+        userService.addUser(user1);
+        Exception exception = assertThrows(UserException.class, ()->
+                userService.updateUser("Username", "newPassword", "newFirstName",
+                        "newLastName", "newAddress", ""));
+
+        String expectedMessage = "Information is not valid";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+
+    @Test
     @DisplayName("updateUser: User does not exist in database")
-    void _8_updateUser_userDoesNotExist_thenSucceedsMessage() {
+    void _9_updateUser_userDoesNotExist_thenSucceedsMessage() {
         userService.addUser(user1);
         Exception exception = assertThrows(UserException.class, ()->
                 userService.updateUser("noUserExists", "newPassword", "newName",
