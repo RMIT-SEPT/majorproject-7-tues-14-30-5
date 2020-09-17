937ab55d671e0c38b63a76784b68229eda097d17
Alan Lam <s3436174@student.rmit.edu.au>
2020-09-17 17:02:38 +1000

    Include redux devtools to allow devtools in browser

--- FrontEnd/frontend-app/src/redux/store.js
+++ FrontEnd/frontend-app/src/redux/store.js
@@ -1,8 +1,9 @@
 import { applyMiddleware, createStore } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
 import rootReducer from './rootReducer'
 import logger from 'redux-logger'
 import thunk from 'redux-thunk'
 
-const store = createStore(rootReducer, applyMiddleware(logger, thunk))
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(logger, thunk)))
 
 export default store
