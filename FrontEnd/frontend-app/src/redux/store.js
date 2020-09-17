a888b0d79a3b7d4cd5cd4489079e68fef826abcd
Alan Lam <s3436174@student.rmit.edu.au>
2020-09-17 17:26:11 +1000

    Implement persisting state

--- FrontEnd/frontend-app/src/redux/store.js
+++ FrontEnd/frontend-app/src/redux/store.js
@@ -4,6 +4,33 @@ import rootReducer from './rootReducer'
 import logger from 'redux-logger'
 import thunk from 'redux-thunk'
 
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(logger, thunk)))
+function saveToLocalStorage(state) {
+    try {
+        const serializedState = JSON.stringify(state)
+        localStorage.setItem('state', serializedState)
+
+    } catch(e) {
+        console.log(e)
+    }
+}
+
+function loadFromLocalStorage() {
+    try {
+        const serializedState = localStorage.getItem('state')
+        if(serializedState === null) return undefined
+        return JSON.parse(serializedState)
+        
+    } catch (e) {
+        console.log(e)
+        return undefined
+        
+    }
+}
+
+const persistedState = loadFromLocalStorage()
+
+const store = createStore(rootReducer, persistedState, composeWithDevTools(applyMiddleware(logger, thunk)))
+
+store.subscribe(() => saveToLocalStorage(store.getState()))
 
 export default store
