5a20445694c11355a22bef9853e8fbcc12f1fa8e
Gaveen Amarapala <s3766914@student.rmit.edu.au>
2020-09-20 02:58:48 +1000

    Adding Routing and Modifyinng Login

--- FrontEnd/frontend-app/src/App.js
+++ FrontEnd/frontend-app/src/App.js
@@ -1,14 +1,49 @@
 import React from 'react';
 import './App.css';
-import Welcome from './components/Welcome'
-import LoginForm from "./components/LoginForm";
+import Welcome from './components/Welcome.js'
+import LoginForm from './components/LoginForm.js';
+import Header from "./components/Header.js";
+import Home from "./components/Home.js";
+import BookingPage from "./components/BookingPage"
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Booking_History from './components/history';
+
 function App() {
   return (
-    <div className="App">
-        <Welcome/>
-        <LoginForm/>
+  <div className="app">
+      <Router>
+        <Header />
+        
+        <Switch>
+          <Route path="/booking">
+            <BookingPage />
+          </Route>
+
+          <Route path='/history' component={Booking_History}/>
+
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+  
+      </ Router>
     </div>
-  );
+    );
+    
 }
 
 export default App;
+{/* Home */}
+
+  {/* Header */}
+
+  {/* Banner */}
+    {/* Search */}
+
+
+  {/* Card */}
+      {/* Search */}
+
+
+
--- FrontEnd/frontend-app/src/components/LoginForm.js
+++ FrontEnd/frontend-app/src/components/LoginForm.js
@@ -3,6 +3,7 @@ import { Button, Form } from "react-bootstrap";
 import Logo from "../Logo.png";
 import axios from "axios";
 import "./loginForm.css";
+
 export default class LoginForm extends Component {
   constructor(props) {
     super(props);
@@ -34,23 +35,7 @@ export default class LoginForm extends Component {
     return (
       // <div className="col-md-5 col-md-offset-2">
       <div className="login">
-        <div className="Main">
-          <div className="Logo"> </div>
-          <div className="LetterA"> A</div>
-          <div className="LetterG"> G</div>
-          <div className="LetterM"> M</div>
-          <div className="LetterE"> E</div>
-          <div className="AGME"> AGME Booking Solution</div>
-          {/* <div className="Home"> Home</div> */}
-          {/* <div className="Contact"> Contact</div> */}
-          {/* <div className="About"> About</div> */}
-          <button class="homeBtn">HOME</button>
-          <button class="AboutBtn">ABOUT</button>
-          <button class="ContactBtn">CONTACT</button>
-        </div>
-
-       
-
+      
         <div className="login__container">
         <img className="loginBox__logo" src={Logo} alt="" />
           <Form onSubmit={this.signIn} id="loginFormId">
