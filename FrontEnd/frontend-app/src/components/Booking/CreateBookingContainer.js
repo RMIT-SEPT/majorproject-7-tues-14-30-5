ff7a162651a175cf23a64a156833895315d8c253
Alan Lam <s3436174@student.rmit.edu.au>
2020-09-19 23:25:57 +1000

    Minor update on CreateBookingContainer

--- FrontEnd/frontend-app/src/components/Booking/CreateBookingContainer.js
+++ FrontEnd/frontend-app/src/components/Booking/CreateBookingContainer.js
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { createBooking } from '../../redux'
+import { useDispatch } from 'react-redux'
 
 function CreateBookingContainer() {
 
@@ -8,14 +10,24 @@ function CreateBookingContainer() {
     //     worker: '',
     //     service: ''
     // })
+    
+    // Dummy booking for test purposes
+    const [newBooking, setNewBooking] = useState({
+        dateTime: '2020-12-25T13:00',
+        customer: 'customer',
+        worker: 'worker',
+        service: 'haircut'
+    })
 
-
+    const dispatch = useDispatch()
+    
+    
 
     return (
         <div>
             <form>
-                <button onSubmit=''>Create Booking</button>
-
+                <input type='text' onChange={e => setNewBooking(...newBooking,)} />
+                <input type='button' onClick={() => dispatch(createBooking(newBooking))} value='Create Booking' />
             </form>
         </div>
     )
